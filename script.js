59e0d352d5f678348cbb97e9c365822578e7bb8d
Trevor Otterson <totterson97@gmail.com>
2020-06-16 14:35:09 -0600

    trying to add buttons and timer

---
+++ script.js
@@ -0,0 +1,38 @@
+var aButton = document.querySelector("#")
+var bButton = document.querySelector("#")
+var cButton = document.querySelector("#")
+
+var totalSeconds = 0
+var secondsElapsed = 0
+var status = "started"
+var interval
+
+getTimePreferences()
+
+function getFormattedMinutes() {
+    var secondsLeft = totalSeconds - secondsElapsed
+    var minutesLeft = Math.floor(secondsLeft / 60)
+    var formattedMinutes
+  
+    if (minutesLeft < 10) {
+      formattedMinutes = "0" + minutesLeft
+    } else {
+      formattedMinutes = minutesLeft
+    }
+  
+    return formattedMinutes
+}
+  
+function getFormattedSeconds() {
+    var secondsLeft = (totalSeconds - secondsElapsed) % 60
+    var formattedSeconds
+  
+    if (secondsLeft < 10) {
+      formattedSeconds = "0" + secondsLeft
+    } else {
+      formattedSeconds = secondsLeft
+    }
+  
+    return formattedSeconds
+}
+
